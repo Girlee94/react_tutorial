c3d53472129c6bd84ff56b07f414e0667010eb0c
LAPTOP-2V82M27P\swk01 <swk01139@gmail.com>
2020-11-21 21:18:02 +0900

    Initial

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,42 +1,22 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+// import Form from './prac/form';
 // import Greeting from './prac/LoginControl';
+// import EssayForm from './prac/essayForm';
+// import FlavorForm from './prac/flaverform';
+// import Reservation from './prac/reservation';
+import Calculator from './prac/calculator';
 
 
-// function Mailbox(props) {
-//   const unreadMessages = props.unreadMessages;
-//   return (
-//     <div>
-//       <h1>Hello!</h1>
-//       {unreadMessages.length > 0 &&
-//         <h2>
-//           You have {unreadMessages.length} unread messages.
-//         </h2>
-//       }
-//     </div>
-//   );
-// }
-
-// const messages = ['React', 'Re: React', 'Re:Re: React'];
-
-function NumberList(props) {
-  const numbers = props.numbers;
-  const listItems = numbers.map((number) =>
-    <li key={number.toString()}>
-      {number}
-    </li>
-  );
-
-  return (
-    <ul>{listItems}</ul>
-  );
-}
-
-const numbers = [1, 2, 3, 4, 5];
 
 ReactDOM.render (
-  // <Mailbox unreadMessages={messages} />,
-  <NumberList numbers={numbers} />,
+  // <Map />,
+  // <Form />,
+  // <EssayForm />,
+  // <FlavorForm />,
+  // <Reservation />,
+  <Calculator />,
   document.getElementById('root')
 );
